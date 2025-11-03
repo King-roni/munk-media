24d067e2ce5afb1567cb67b0178724ca57446598
king-roni <pepvanerptk@gmail.com>
2025-11-03 19:30:20 +0200

    fix(socials): update LinkedIn link to company profile

--- lib/socials.ts
+++ lib/socials.ts
@@ -1,6 +1,6 @@
 export const SOCIAL_LINKS = {
   instagram: "https://www.instagram.com/munkmediaagency/",
-  linkedin: "https://www.linkedin.com/in/matisse-unkel-242692395/",
+  linkedin: "https://www.linkedin.com/in/munk-media-242692395/",
   tiktok: "https://www.tiktok.com/@munk.media?_r=1&_t=ZN-915mgT7pcmc",
   x: "https://x.com/MediaMunk",
 } as const;
